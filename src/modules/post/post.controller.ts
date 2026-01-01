013ba71febe2ff8a3a5e88457ee157e51f60448f
TheRABU <mdfazlerabbi114@gmail.com>
2026-01-01 19:13:25 +0600

    added CRUD functions,controllers,routes for post...

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -32,6 +32,94 @@ const createPost = async (req: Request, res: Response, next: NextFunction) => {
   }
 };
 
+const getAllPosts = async (req: Request, res: Response) => {
+  try {
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const search = (req.query.search as string) || "";
+    const isFeatured = req.query.isFeatured
+      ? req.query.isFeatured === "true"
+      : undefined;
+    const tags = req.query.tags ? (req.query.tags as string).split(",") : [];
+
+    const result = await PostServices.getAllPosts({
+      page,
+      limit,
+      search,
+      isFeatured,
+      tags,
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "fetched All Posts successfully!",
+      result,
+    });
+  } catch (error: any) {
+    console.log("error at getAllPosts::", error.message);
+    return res.status(500).json({
+      success: false,
+      message: "could not get posts error occurred!",
+    });
+  }
+};
+
+const getPostById = async (req: Request, res: Response) => {
+  try {
+    const post = await PostServices.getPostById(Number(req.params.id));
+    if (!post) return res.status(404).json({ error: "Post not found" });
+    res.status(200).json({
+      success: true,
+      message: "fetched post data successfully!",
+      post,
+    });
+  } catch (error: any) {
+    console.log("error at getPostById::", error.message);
+    return res.status(500).json({
+      success: false,
+      message: "could not get post details error occurred!",
+    });
+  }
+};
+const updatePost = async (req: Request, res: Response) => {
+  try {
+    const post = await PostServices.updatePost(Number(req.params.id), req.body);
+    res.status(200).json({
+      success: true,
+      message: "updated post data successfully!",
+      post,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "could not update post details error occurred!",
+    });
+  }
+};
+
+const deletePost = async (req: Request, res: Response) => {
+  await PostServices.deletePost(Number(req.params.id));
+  res.json({ message: "Post deleted" });
+};
+
+const getBlogStat = async (req: Request, res: Response) => {
+  try {
+    const result = await PostServices.getBlogStat();
+    res.status(200).json({
+      success: true,
+      message: "Data fetched for stat of blog posts",
+      result,
+    });
+  } catch (err) {
+    res.status(500).json({ error: "Failed to fetch stats", details: err });
+  }
+};
+
 export const PostControllers = {
   createPost,
+  getAllPosts,
+  getPostById,
+  updatePost,
+  deletePost,
+  getBlogStat,
 };
--- src/modules/post/post.routes.ts
+++ src/modules/post/post.routes.ts
@@ -5,6 +5,12 @@ import { Role } from "../../types/User";
 
 const postRouter = Router();
 
+postRouter.get("/stats", PostControllers.getBlogStat);
+
 postRouter.post("/", checkAuth(Role.USER), PostControllers.createPost);
+postRouter.get("/", PostControllers.getAllPosts);
+postRouter.get("/:id", PostControllers.getPostById);
+postRouter.patch("/:id", PostControllers.updatePost);
+postRouter.delete("/:id", PostControllers.deletePost);
 
 export default postRouter;
